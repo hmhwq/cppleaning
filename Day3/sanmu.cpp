88d74c23b3f7f0ed23e0f887b3f8c3d7533acad6
hmhwq <2682783002@qq.com>
2026-02-12 23:14:41 +0800

    Day3

---
+++ Day3/sanmu.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+#define bigger() a>b?a:b ;
+int main()
+{
+
+}
---
+++ Day3/sanzhixioazhu.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int num1 = 0,num2 = 0,num3 = 0;
+
+    cout << "请输入小猪A的重量"<<endl;
+    cin>> num1 ;
+
+    cout << "请输入小猪B的重量"<<endl;
+    cin>> num2 ;
+
+    cout << "请输入小猪C的重量"<<endl;
+    cin>> num3 ;
+
+
+    cout<<"小猪A的体重为：" << num1 << endl;
+    cout<<"小猪B的体重为：" << num2 << endl;
+    cout<<"小猪C的体重为：" << num3 << endl;   
+
+
+    if (num1 > num2)
+    {
+        if (num1>num3)
+        {
+            cout << "小猪A最重"<<endl;
+
+        }
+        else{cout << "小猪C最重"<<endl;}
+        
+    }
+    else
+    {
+        if (num2>num3)
+        {
+            cout << "小猪B最重"<<endl;
+
+        }
+        else{cout << "小猪C最重"<<endl;}
+    }
+
+    system("pause");
+    return 0;
+
+}
